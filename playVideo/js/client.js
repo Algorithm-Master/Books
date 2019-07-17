75efae2a4244643d141477112149367107dfc53f
rickli <382542165@qq.com>
2019-07-17 18:47:57 +0800

    handle error

--- playVideo/js/client.js
+++ playVideo/js/client.js
@@ -8,13 +8,13 @@ const localVideo = document.querySelector('video');
 
 function gotLocalMediaStream(mediaStream){
     console.log('navigator.getUserMedia success');
-    window.webkit.messageHandlers.logger.postMessage('navigator.getUserMedia success')
+    // window.webkit.messageHandlers.logger.postMessage('navigator.getUserMedia success')
     localVideo.srcObject = mediaStream;
 }
 
 function handleLocalMediaStreamError(error){
     alert('navigator.getUserMedia error: '+ error.message)
-    window.webkit.messageHandlers.logger.postMessage(error.message)
+    // window.webkit.messageHandlers.logger.postMessage(error.message)
     console.log('navigator.getUserMedia error: ', error.message);
 }
 
