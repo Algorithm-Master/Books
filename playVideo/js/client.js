d7c45923154d8a39e24b6eeee16fb130e0b3da47
rickli <382542165@qq.com>
2019-07-18 15:41:55 +0800

    commit files

--- playVideo/js/client.js
+++ playVideo/js/client.js
@@ -1,8 +1,32 @@
 'use strict';
 
+// 判断浏览器是否支持这些 API
+if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+    console.log("enumerateDevices() not supported.");
+}
+
+let videoId = '';
+// 枚举 cameras and microphones.
+navigator.mediaDevices.enumerateDevices()
+    .then(function(deviceInfos) {
+
+        // 打印出每一个设备的信息
+        deviceInfos.forEach(function(deviceInfo) {
+            console.log(deviceInfo.kind + ": " + deviceInfo.label +
+                " id = " + deviceInfo.deviceId);
+            if(deviceInfo.kind === 'videoinput') {
+                videoId = deviceInfo.deviceId
+            }
+        });
+    })
+    .catch(function(err) {
+        console.log(err.name + ": " + err.message);
+    });
+
 const mediaStreamContrains = {
     video: {
-        facingMode: "environment",
+        deviceID: videoId,
+        // facingMode: "user",
         width: 320,
         height: 200
     },
